4dd3758d05a451be2dbc7b005010bc7b8d7bb361
jiaweitan <gamilsphat@gmail.com>
2020-05-29 15:24:29 -0700

    Enable RESTFUL swagger

--- src/main/java/com/springbootblog/springbootblog/SpringBootBlogApplication.java
+++ src/main/java/com/springbootblog/springbootblog/SpringBootBlogApplication.java
@@ -3,9 +3,11 @@ package com.springbootblog.springbootblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class SpringBootBlogApplication {
 
     public static void main(String[] args) {
