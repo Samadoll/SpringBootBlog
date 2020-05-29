8bef2c9c9301434eeca3166445eb56a4b82891a8
jiaweitan <gamilsphat@gmail.com>
2020-05-29 01:29:56 -0700

    Setup Security

---
+++ src/main/java/com/springbootblog/springbootblog/security/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.springbootblog.springbootblog.security;
+
+import com.springbootblog.springbootblog.entity.ResponseEntity;
+import com.springbootblog.springbootblog.util.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    // Setup AuthenticationManager
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super();
+        setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // Get username and password from post request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        setDetails(request, token);
+        // User AuthenticationManager to Authenticate
+        return getAuthenticationManager().authenticate(token);
+    }
+
+    // Send back encrypted token after successful authentication
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        handleResponse(request, response, authResult, null);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException failed) throws IOException, ServletException {
+        handleResponse(request, response, null, failed);
+    }
+
+    private void handleResponse(HttpServletRequest request, HttpServletResponse response, Authentication authResult,
+                                AuthenticationException failed) throws IOException, ServletException {
+        // User Spring Jackson to convert Entity to Json
+        ObjectMapper mapper = new ObjectMapper();
+        ResponseEntity responseEntity = new ResponseEntity();
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
+        if (authResult != null) {
+            // Successful request
+            User user = (User) authResult.getPrincipal();
+            String token = JwtUtil.sign(user.getUsername(), user.getPassword());
+            responseEntity.setStatus(HttpStatus.OK.value());
+            responseEntity.setMessage("Successfully Logged In");
+            responseEntity.setData("bearer " + token);
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            responseEntity.setStatus(HttpStatus.BAD_REQUEST.value());
+            responseEntity.setMessage("Invalid Username Or Password");
+            responseEntity.setData(null);
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
+        response.getWriter().write(mapper.writeValueAsString(responseEntity));
+    }
+}
---
+++ src/main/java/com/springbootblog/springbootblog/security/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.springbootblog.springbootblog.security;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserCache;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.cache.SpringCacheBasedUserCache;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.Assert;
+import java.lang.reflect.Constructor;
+import java.util.Objects;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                // disable csrf
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), cachingUserDetailsService(userDetailsService)))
+                // Make Stateless
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        CachingUserDetailsService cachingUserDetailsService = cachingUserDetailsService(userDetailsService);
+        UserCache userCache = new SpringCacheBasedUserCache(Objects.requireNonNull(cacheManager.getCache("jwt-cache")));
+        cachingUserDetailsService.setUserCache(userCache);
+        auth.eraseCredentials(false);
+        auth.userDetailsService(cachingUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private CachingUserDetailsService cachingUserDetailsService(UserDetailsServiceImpl delegate) {
+        Constructor<CachingUserDetailsService> constructor = null;
+        try {
+            constructor = CachingUserDetailsService.class.getDeclaredConstructor(UserDetailsService.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        Assert.notNull(constructor, "CachingUserDetailsService constructor is null");
+        constructor.setAccessible(true);
+        return BeanUtils.instantiateClass(constructor, delegate);
+    }
+}
