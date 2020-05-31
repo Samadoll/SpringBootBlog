57791fac94d3d435ef021886cf7fa186a32fd1f1
jiaweitan <gamilsphat@gmail.com>
2020-05-30 18:21:10 -0700

    configre webpack, add TicTacToe game as test

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    entry: './frontend/src/app.js',
+    devtool: 'source-map',
+    cache: true,
+    mode: 'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }]
+            }
+        ]
+    }
+};
