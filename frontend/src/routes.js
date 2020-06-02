89696925e788a1457df2d65d4415f1552f04bb35
jiaweitan <gamilsphat@gmail.com>
2020-06-02 15:21:24 -0700

    route initial fetch and loading

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {HashRouter, Switch, Route } from "react-router-dom";
+import {HashRouter, Switch, Route, Redirect} from "react-router-dom";
 import Home from "./page/home"
 import { Login } from "./page/login";
 import { Register } from "./page/register";
@@ -26,53 +26,27 @@ export function Routes() {
         setIsLoggedIn(false);
     }
 
-    // async function initialFetch() {
-    //     Axios.defaults.headers.Authorization = localStorage.getItem("Authorization") || "";
-    //     await Axios.get("/api/user/checkAuth")
-    //             .then((res) => {
-    //                 const status = res.status;
-    //                 if (status === 200) {
-    //                     setUserInfo(res.data);
-    //                     setIsLoggedIn(true);
-    //                 }
-    //                 setIsLoading(false);
-    //             })
-    //             .catch((e) => {
-    //                 setIsLoading(false);
-    //             })
-    // }
+    async function initialFetch() {
+        const token = localStorage.getItem("Authorization") || "";
+        if (token !== "") {
+            try {
+                Axios.defaults.headers.Authorization = localStorage.getItem("Authorization") || "";
+                const res = await Axios.get("/api/user/checkAuth");
+                const status = res.status;
+                if (status === 200) {
+                    setUserInfo({username: res.data.data.username});
+                    setIsLoggedIn(true);
+                }
+            } catch (e) {
+                // ignored
+            }
+        }
+        setIsLoading(false);
+    }
 
-    // useEffect(() => {
-    //     setIsLoading(true);
-    //     initialFetch();
-    // })
-
-    // return isLoading
-    //     ? (
-    //         <div className="loading-container">
-    //             <div className="opblock-loading-animation">
-    //                 <Spinner marginX="auto" size={100}/>
-    //             </div>
-    //         </div>
-    //     )
-    //     : (
-    //         <HashRouter>
-    //             <Header
-    //                 isLoggedIn={isLoggedIn}
-    //                 userInfo={userInfo}
-    //                 logout={logout}
-    //             />
-    //             <div className="content">
-    //                 <Switch>
-    //                     <Route exact path="/" component={Home}/>
-    //                     <Route path="/loginPage">
-    //                         <Login login={login} />
-    //                     </Route>
-    //                     <Route exact path="/registerPage" component={Register}/>
-    //                 </Switch>
-    //             </div>
-    //         </HashRouter>
-    //     );
+    useEffect(() => {
+        initialFetch();
+    }, [])
 
     return (
         <HashRouter>
@@ -82,14 +56,32 @@ export function Routes() {
                 logout={logout}
             />
             <div className="content">
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/loginPage">
-                        <Login login={login} />
-                    </Route>
-                    <Route exact path="/registerPage" component={Register}/>
-                </Switch>
+                {
+                    isLoading
+                        ? (
+                            <div className="loading-container">
+                                <div className="opblock-loading-animation">
+                                    <Spinner marginX="auto" size={100}/>
+                                </div>
+                            </div>
+                        )
+                        : (
+                            <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route path="/loginPage">
+                                    {
+                                        isLoggedIn ? (<Redirect to="/" />) : (<Login login={login} />)
+                                    }
+                                </Route>
+                                <Route exact path="/registerPage" >
+                                    {
+                                        isLoggedIn ? (<Redirect to="/" />) : (<Register />)
+                                    }
+                                </Route>
+                            </Switch>
+                        )
+                }
             </div>
         </HashRouter>
-    )
+    );
 }
