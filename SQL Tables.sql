654d9237940ab845a59915ab6d7255ba263b2aed
jiaweitan <gamilsphat@gmail.com>
2020-06-04 14:40:18 -0700

    update sql table, and add comments table

--- SQL Tables.sql
+++ SQL Tables.sql
@@ -4,42 +4,45 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for content
 -- ----------------------------
-DROP TABLE IF EXISTS `Content`;
-CREATE TABLE `Content` (
+DROP TABLE IF EXISTS `content`;
+CREATE TABLE `content` (
   `cid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `content` text COLLATE utf8mb4_general_ci NOT NULL,
   `author_id` int(11) NOT NULL,
   `create_time` bigint(13) NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for relationships
 -- ----------------------------
-DROP TABLE IF EXISTS `Relationships`;
-CREATE TABLE `Relationships` (
+DROP TABLE IF EXISTS `relationships`;
+CREATE TABLE `relationships` (
   `cid` int(11) NOT NULL,
   `tid` int(11) NOT NULL,
-  UNIQUE KEY `tid_cid_unique` (`tid`,`cid`)
+  UNIQUE KEY `tid_cid_unique` (`tid`,`cid`),
+  FOREIGN KEY (`cid`)
+    REFERENCES content (`cid`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for tags
 -- ----------------------------
-DROP TABLE IF EXISTS `Tags`;
-CREATE TABLE `Tags` (
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
   `tid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`tid`),
   UNIQUE KEY `name_unique` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `User`;
-CREATE TABLE `User` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
@@ -47,6 +50,23 @@ CREATE TABLE `User` (
   `register_time` bigint(13) NOT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username_unique` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for Comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11),
+  `contentId` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `comment` text COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` bigint(13) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`contentId`)
+    REFERENCES content (`cid`)
+    ON DELETE CASCADE,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
