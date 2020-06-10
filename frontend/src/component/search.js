7598cf56c934ce8c8f90f55d6fb8c23e990df6c8
jiaweitan <gamilsphat@gmail.com>
2020-06-10 16:47:16 -0700

    add reset button to the search

--- frontend/src/component/search.js
+++ frontend/src/component/search.js
@@ -1,17 +1,21 @@
 import React, {useState} from "react";
-import {SearchIcon} from "evergreen-ui";
+import {SearchIcon, ResetIcon} from "evergreen-ui";
 
 export function Search(props) {
     const [searchString, setSearchString] = useState("");
 
-    function handleSearch() {
-        props.searchFunction.apply(null, [searchString]);
+    function handleSearch(search) {
+        setSearchString(search);
+        props.searchFunction.apply(null, [search]);
     }
 
     return (
         <div className={props.className}>
-            <input type={"text"} placeholder={"Search..."} onChange={e => setSearchString(e.target.value)} />
-            <button onClick={() => handleSearch()}>
+            <button onClick={() => handleSearch("")} className={`${props.className}-button-front`}>
+                <ResetIcon size={20} color={"muted"}/>
+            </button>
+            <input type={"text"} placeholder={"Search..."} onChange={e => setSearchString(e.target.value)} value={searchString}/>
+            <button onClick={() => handleSearch(searchString)} className={`${props.className}-button-back`}>
                 <SearchIcon size={20} color={"muted"}/>
             </button>
         </div>
